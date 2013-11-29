1a8b78c3cf8cb270ea5bdd16fb0907eb68867a0c
Brian Clarke <brian.clarke@students.ittralee.ie>
2013-11-29 18:12:22 +0000

    3 Test Checkin

---
+++ UnityProto3/ClassTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+	class ClassTest
+	{
+        ///
+        /// This is a test comment in a test class
+        ///
+	}
+
